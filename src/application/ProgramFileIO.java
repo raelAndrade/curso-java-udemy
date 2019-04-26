9e672fbdb09206a7a751595ed32997d74b2edbba
Israel Andrade <israelg.andrade@bol.com.br>
2019-04-26 00:39:52 -0300

    atualizando branch master

--- src/application/ProgramFileIO.java
+++ src/application/ProgramFileIO.java
@@ -9,7 +9,6 @@ public class ProgramFileIO {
 	public static void main(String[] args) {
 		
 		File file = new File("/home/israel/Documentos/teste.txt");
-		System.out.println("File: "+ file);
 		Scanner sc = null;
 		
 		try {
@@ -21,7 +20,6 @@ public class ProgramFileIO {
 			System.out.println("Error opening file: " + e.getMessage());
 		}finally {
 			if (sc != null) {
-				// Fecha o Scanner
 				sc.close();
 			}
 			System.out.println("Finally block executed");
